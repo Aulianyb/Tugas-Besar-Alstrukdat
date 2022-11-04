1758b15bddc4a3d8968bebe088b085e116f35f51
SHaNM3 <farhan.alganiputra@gmail.com>
2022-11-04 15:51:30 +0700

    Added Create Game header

---
+++ Create_Game.h
@@ -0,0 +1,15 @@
+#ifndef CREATEGAME_H
+#define CREATEGAME_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "array.h"
+#include "boolean.h"
+#include "mesinkata.h"
+
+void CreateGame(TabGame* List_Game);
+/* I.S. TabGame List_Game terdefinisi */
+/* F.S. Game baru hasil masukan pengguna berhasil ditambahkan ke dalam TabGame List_Game */
+
+#endif
