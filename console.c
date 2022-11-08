86439a0e3dd1c2e53869ac02335c60a32548271f
hilmibaskara <hilmibaskara@gmail.com>
2022-11-09 00:24:45 +0700

    start save load quit done

---
+++ console.c
@@ -0,0 +1,100 @@
+#include "console.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void quit(){
+    printf("Terima kasih telah memainkam game BNMO ini!\n");
+    printf("Good Game, Well Played\n");
+    exit(0);
+}
+
+void startLOAD(char* filename)
+{
+    fptr = fopen(filename, "r");
+    ADVLOAD();
+}
+
+void IgnoreFILEMARK()
+{
+    while (currentChar == FILEMARK){
+        ADVLOAD();
+    }
+}
+
+void ADVWORDLOAD()
+{
+    IgnoreFILEMARK();
+    if (retval < 0) EndWord = true;
+    else CopyWORDLOAD();
+}
+
+void CopyWORDLOAD()
+{
+    int i = 0;
+    while ((currentChar != FILEMARK) && i < NMax && !EOP) {
+        currentWord.TabWord[i] = currentChar; 
+        ADVLOAD();
+        i++;
+    }
+    currentWord.Length = i;
+}
+
+void ADVLOAD()
+{
+	retval = fscanf(fptr, "%c", &currentChar);
+	EOP = (retval < 0);
+	if (EOP) {
+        fclose(fptr);
+ 	}
+}
+
+char* wordToString(Word word)
+{
+  char *str = (char *)malloc(sizeof(char) * word.Length);
+  int i;
+  for (i = 0; i < word.Length; i++)
+  {
+    str[i] = word.TabWord[i];
+  }
+  str[i] = '\0';
+  return str;
+}
+
+void startGAME(TabGame *listGame){
+    loadGAME("config.txt", listGame);
+    printf("File konfigurasi BNMO berhasil dibaca. GLHF!!\n");
+}
+
+void loadGAME(char* filename, TabGame *listGame)
+{
+    startLOAD(filename);
+    int countGame = currentChar - '0';
+
+    ADVLOAD();
+    char* title;
+    for (int i = 0; i < countGame; i++)
+    {
+        ADVWORDLOAD();
+        title = wordToString(currentWord);
+        SetEl(listGame, i, title);
+    }
+    fclose(fptr);
+}
+
+void saveGAME(char* filename, TabGame listGame){
+    fptr = fopen(filename, "w");
+    if (fptr == NULL){
+        printf("Tidak berhasil menyimpan file!");
+    }
+    else
+    {
+        fprintf(fptr, "%d\n", listGame.Neff);
+        for (int i = 0; i < listGame.Neff-1; i++)
+        {
+            fprintf(fptr, "%s\n", listGame.TI[i]);   
+        }
+        fprintf(fptr, "%s", listGame.TI[listGame.Neff-1]);
+        fclose(fptr);
+        printf("Berhasil melakukan save\n");
+    }
+}
---
+++ console.h
@@ -0,0 +1,42 @@
+#ifndef CONSOLE_H
+#define CONSOLE_H
+
+#include "mesinkata.h"
+#include "mesinkarakter.h"
+#include "array.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define FILEMARK '\n'
+
+static FILE *fptr;
+static int retval;
+
+void quit();
+/* keluar dari program */
+
+/* ----- IMPLEMENTASI MESINKATA UNTUK FUNGSI LOAD -----*/
+void startLOAD(char* filename);
+ 
+void IgnoreFILEMARK();
+
+void ADVWORDLOAD();
+
+void CopyWORDLOAD();
+
+void ADVLOAD();
+
+/* ----- IMPLEMENTASI FILE HANDLING DALAM PROGRAM -----*/
+char* wordToString(Word word);
+// Melakukan convert dari ADT Word ke string
+
+void loadGAME(char* filename, TabGame *listGame);
+// Melakukan pembacaan file
+
+void startGAME(TabGame *listGame);
+// Melakukan pembacaan file config
+
+void saveGAME(char* filename, TabGame listGame);
+// Melakukan penyimpanan pada file
+
+#endif
