4a86b333a2877f4018e2f3a36f2e2b610f7f14dc
SHaNM3 <farhan.alganiputra@gmail.com>
2022-11-08 19:48:21 +0700

    added help header

---
+++ help.h
@@ -0,0 +1,12 @@
+#ifndef HELP_H
+#define HELP_H
+
+void help(int state);
+/*
+I.S. Sembarang
+F.S. Menampilkan seluruh command yang dapat dipanggil di dalam sistem
+     berdasarkan statenya. Bila state = 0, maka hanya menampilkan START. Bila
+     state = 1, menampilkan sisa command yang lain.
+*/
+
+#endif
