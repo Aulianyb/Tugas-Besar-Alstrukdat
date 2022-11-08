2a957e4145a433115ede613918c12a0a7e881232
SHaNM3 <farhan.alganiputra@gmail.com>
2022-11-08 19:48:41 +0700

    updated list game

--- List_Game.c
+++ List_Game.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-#include "array.h"
 #include "List_Game.h"
 
 void ListGame(TabGame List_Game) {
+/*
+I.S. Sembarang
+F.S. Menampilkan seluruh game yang tersedia di dalam sistem.
+*/
     printf("Berikut adalah daftar game yang tersedia :\n");
     for (int i = IdxMin; i <= List_Game.Neff; i++) {
         printf("%d. ",i);
