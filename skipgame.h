733601421421e674e29e1359f253e02d9063377d
rifqisyafiq <89325784+rifqisyafiq@users.noreply.github.com>
2022-11-09 21:05:19 +0700

    Update skipgame.h

--- skipgame.h
+++ skipgame.h
@@ -1,15 +1,15 @@
-#include "queue.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include "queue.h"
-#include "RNG.h"
-#include "Dinner_Dash.h"
-#include "RockPaperScissor.h"
-
 #ifndef SKIPGAME_H
 #define SKIPGAME_H
 
-void skipgame(Queue game_queue, int n);
+#include "queue.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "boolean.h"
+#include "Dinner_dash.h"
+#include "rng.h"
+#include "RockPaperScissors.h"
+
+void SkipGame(Queue game_queue, int n);
 
 // I.S Sembarang
 // F.S Menampilkan seluruh game yang terdapat di queue dan memainkan game sesuai dengan input user mengenai banyaknya game yang di skip
