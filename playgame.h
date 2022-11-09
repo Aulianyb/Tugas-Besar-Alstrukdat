acc4bb14da87b6d2cbe678b43051ab022fcfed72
rifqisyafiq <89325784+rifqisyafiq@users.noreply.github.com>
2022-11-09 09:33:56 +0700

    Header Skipgame dan playgame

---
+++ playgame.h
@@ -0,0 +1,18 @@
+#include "queue.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+#include "RNG.h"
+#include "Diner DASH.h"
+#include "RockPaperScissors.h"
+
+
+#ifndef playgame_h
+#define playgame_h
+
+void playgame(Queue Queue_game);
+
+// I.S. Sembarang
+// F.S. Menampilkan seluruh game yang terdapat di queue dan memainkan game yang pertama muncul di queue
+
+#endif
---
+++ skipgame.h
@@ -0,0 +1,17 @@
+#include "queue.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+#include "RNG.h"
+#include "Diner DASH.h"
+#include "RockPaperScissors.h"
+
+#ifndef SKIPGAME_H
+#define SKIPGAME_H
+
+void skipgame(Queue Queue_game, int n);
+
+// I.S Sembarang
+// F.S Menampilkan seluruh game yang terdapat di queue dan memainkan game sesuai dengan input user mengenai banyaknya game yang di skip
+
+#endif
