d9edea2c1e6b751f7ad3821cdccad848a197add0
rifqisyafiq <89325784+rifqisyafiq@users.noreply.github.com>
2022-11-10 07:55:34 +0700

    Update playgame.c

--- playgame.c
+++ playgame.c
@@ -7,22 +7,22 @@
 void PlayGame(Queue game_queue){
     printf("Berikut ini adalah daftar Game-mu : \n");
     PrintQueueGame(game_queue);
-    char *val;
+    Word val;
     dequeue(&game_queue, &val);
     printf("Game yang akan dimainkan adalah %s\n", val);
-    if (isEqual(val, "Diner DASH"))
+    if (isWordEqual(val, "Diner DASH"))
     {
         printf("Loading %s .....\n",val);
         // PLay Diner DASH
         Dinner_Dash();
     }
-    else if (isEqual(val, "RNG"))
+    else if (isWordEqual(val, "RNG"))
     {
         printf("Loading %s .....\n",val);
         // Play RNG
         RNG();
     }
-    else if (isEqual(val, "RockPaperScissor"))
+    else if (isWordEqual(val, "RockPaperScissors"))
     {
         printf("Loading %s .....\n",val);
         // Play RockPaperScissors PLACEHOLDER
