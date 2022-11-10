1302d9344486b6743c104612f79dc78d371eed75
SHaNM3 <farhan.alganiputra@gmail.com>
2022-11-10 11:12:00 +0700

    updated help format

--- src/Function/Help/help.c
+++ src/Function/Help/help.c
@@ -11,21 +11,25 @@ F.S. Menampilkan seluruh command yang dapat dipanggil di dalam sistem
 */
     printf("Daftar Command yang dapat dipanggil : \n");
     if (state == 0) {
-        printf("================== HELP ===================\n");
-        printf("1. START - Membaca file konfigurasi default\n");
+        printf("==================================== HELP ====================================\n");
+        printf("No  | Daftar Command | Keterangan\n");
+        printf("------------------------------------------------------------------------------\n");
+        printf("1.  | START          | Membaca file konfigurasi default\n");
+        printf("2.  | LOAD           | Membaca save file yang berisi list game dan histori\n");
     }
     else {
-        printf("==================================== HELP ====================================\n");
-        printf("1. LOAD - Membaca save file yang berisi list game dan histori\n");
-        printf("2. SAVE - Menyimpan state game pemain saat ini ke dalam save file\n");
-        printf("3. CREATE GAME - Menambahkan game baru pada daftar game\n");
-        printf("4. LIST GAME - Menampilkan daftar game yang disediakan oleh sistem\n");
-        printf("5. START - Membaca file konfigurasi default\n");
-        printf("6. DELETE GAME - Menghapus sebuah game dari daftar game\n");
-        printf("7. QUEUE GAME - Mendaftarkan permainan ke dalam list game yang akan dimainkan\n");
-        printf("8. PLAY GAME - Memainkan sebuah permainan\n");
-        printf("9. SKIPGAME X - Melewatkan permainan sebanyak X\n");
-        printf("10. QUIT - Keluar dari program\n");
-        printf("11. HELP - Menampilkan daftar command yang dapat dipanggil\n");
+        printf("======================================= HELP =======================================\n");
+        printf("No  | Daftar Command | Keterangan\n");
+        printf("------------------------------------------------------------------------------------\n");
+        printf("1.  | SAVE           | Menyimpan state game pemain saat ini ke dalam save file\n");
+        printf("2.  | CREATE GAME    | Menambahkan game baru pada daftar game\n");
+        printf("3.  | LIST GAME      | Menampilkan daftar game yang disediakan oleh sistem\n");
+        printf("4.  | START          | Membaca file konfigurasi default\n");
+        printf("5.  | DELETE GAME    | Menghapus sebuah game dari daftar game\n");
+        printf("6.  | QUEUE GAME     | Mendaftarkan permainan ke dalam list game yang akan dimainkan\n");
+        printf("7.  | PLAY GAME      | Memainkan sebuah permainan\n");
+        printf("8.  | SKIPGAME X     | Melewatkan permainan sebanyak X\n");
+        printf("9.  | QUIT           | Keluar dari program\n");
+        printf("10. | HELP           | Menampilkan daftar command yang dapat dipanggil\n");
     }
 }
