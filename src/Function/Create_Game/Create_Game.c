f2236aed98a53bfb0cc2e72de1b2c122b8b3b13f
SHaNM3 <farhan.alganiputra@gmail.com>
2022-11-11 17:45:19 +0700

    Updated create game

--- src/Function/Create_Game/Create_Game.c
+++ src/Function/Create_Game/Create_Game.c
@@ -5,36 +5,18 @@ void CreateGame(TabGame* List_Game) {
 /* F.S. Game baru hasil masukan pengguna berhasil ditambahkan ke dalam TabGame List_Game */
     printf("Masukkan nama game yang akan ditambahkan: ");
     STARTFILE();
-
-    //Word added;
-    //int trav = 0;
-    //for (trav = 0; trav < currentWord.Length; trav++) {
-    //    if (currentWord.TabWord[trav] != '\0') {
-    //        added.TabWord[trav] = currentWord.TabWord[trav];
-    //    }
-    //}
-    //added.Length = currentWord.Length;
     
     int i = IdxMin;
     boolean found = false;
     while (i <= (*List_Game).Neff && !found) {
-        int k = 0;
-        boolean equal = true;
-        while ((*List_Game).TI[i].TabWord[k] != '\0' && currentWord.TabWord[k] != '\0' && equal) {
-            if ((*List_Game).TI[i].TabWord[k] != currentWord.TabWord[k]) {
-                equal = false;
-            }
-            else {
-                k++;
-            }
-        }
-        if (equal) {
+        if (isTwoWordEqual((*List_Game).TI[i], currentWord)) {
             found = true;
         }
         else {
             i++;
         }
     }
+
     if (!found) {
         SetEl(List_Game,i,currentWord);
         printf("Game berhasil ditambahkan\n");
