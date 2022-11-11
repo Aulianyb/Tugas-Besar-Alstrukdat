6aa5ccc69ea992d9665c8b3541879961c4c56577
akmalnatha <akmalabhirama2002@gmail.com>
2022-11-11 20:19:30 +0700

    MAKEFILE

--- src/Function/Skip_Game/skipgame.c
+++ src/Function/Skip_Game/skipgame.c
@@ -10,57 +10,57 @@ void SkipGame(Queue *game_queue, int n){
         printf("Belum ada game yang di queue!\n");
     }
     else if (n >= panjang) {
-    printf("Tidak ada permainan lagi dalam daftar Game-mu.\n");
-    Word val;
-    dequeue(game_queue, &val);
-    for (int i = 0; i < n; i++) {
+        printf("Tidak ada permainan lagi dalam daftar Game-mu.\n");
+        Word val;
         dequeue(game_queue, &val);
-    }
+        for (int i = 0; i < n; i++) {
+            dequeue(game_queue, &val);
+        }
     }
     else {
-    Word val;
-    dequeue(game_queue, &val);
-    for (int i = 0; i < n; i++) {
+        Word val;
         dequeue(game_queue, &val);
+        for (int i = 0; i < n; i++) {
+            dequeue(game_queue, &val);
+        }
+        
+        if (isWordEqual(val, "Diner Dash"))
+        {
+            printf("Loading ");
+            PrintKata(val);
+            printf("....\n");
+            // PLay Diner DASH
+            Dinner_Dash();
+        }
+        else if (isWordEqual(val, "RNG"))
+        {
+            printf("Loading ");
+            PrintKata(val);
+            printf("....\n");
+            // Play RNG
+            RNG();
+        }
+        else if (isWordEqual(val, "RockPaperScissor"))
+        {
+            printf("Loading ");
+            PrintKata(val);
+            printf("....\n");
+            RockPaperScissor();
+        }
+        else if (isWordEqual(val, "DINOSAUR IN EARTH") || isWordEqual(val, "RISEWOMAN") || isWordEqual(val, "EIFFEL TOWER")){
+            printf("Game ");
+            PrintKata(val);
+            printf(" masih dalam maintenance, belum dapat dimainkan. Silahkan pilih game lain.\n");
+        }
+        else {
+            printf("Loading ");
+            PrintKata(val);
+            printf("....\n");
+            printf("  _____ _____ _____ _____    _____ _____ _____ _____ \n");
+            printf(" |   __|  _  |     |   __|  |     |  |  |   __| __  |\n");
+            printf(" |  |  |     | | | |   __|  |  |  |  |  |   __|    -|\n");
+            printf(" |_____|__|__|_|_|_|_____|  |_____|\\___/|_____|__|__|\n\n");
+            printf("SCORE AKHIR : %d\n", GenRand(100,100,2));
+        }
     }
-    
-    if (isWordEqual(val, "Diner Dash"))
-    {
-        printf("Loading ");
-        PrintKata(val);
-        printf("....\n");
-        // PLay Diner DASH
-        Dinner_Dash();
-    }
-    else if (isWordEqual(val, "RNG"))
-    {
-        printf("Loading ");
-        PrintKata(val);
-        printf("....\n");
-        // Play RNG
-        RNG();
-    }
-    else if (isWordEqual(val, "RockPaperScissor"))
-    {
-        printf("Loading ");
-        PrintKata(val);
-        printf("....\n");
-        RockPaperScissor();
-    }
-    else if (isWordEqual(val, "DINOSAUR IN EARTH") || isWordEqual(val, "RISEWOMAN") || isWordEqual(val, "EIFFEL TOWER")){
-        printf("Game ");
-        PrintKata(val);
-        printf(" masih dalam maintenance, belum dapat dimainkan. Silahkan pilih game lain.\n");
-    }
-    else {
-        printf("Loading ");
-        PrintKata(val);
-        printf("....\n");
-        printf("  _____ _____ _____ _____    _____ _____ _____ _____ \n");
-        printf(" |   __|  _  |     |   __|  |     |  |  |   __| __  |\n");
-        printf(" |  |  |     | | | |   __|  |  |  |  |  |   __|    -|\n");
-        printf(" |_____|__|__|_|_|_|_____|  |_____|\\___/|_____|__|__|\n\n");
-        printf("SCORE AKHIR : %d\n", GenRand(100,100,2));
-    }
-}
 }
