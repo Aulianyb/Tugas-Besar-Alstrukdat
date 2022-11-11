7f99705eaac8bd145d3d9a4c29b3607c0c6adcd0
SHaNM3 <farhan.alganiputra@gmail.com>
2022-11-11 17:45:48 +0700

    Updated list game

--- src/Function/List_Game/List_Game.c
+++ src/Function/List_Game/List_Game.c
@@ -7,10 +7,5 @@ I.S. Sembarang
 F.S. Menampilkan seluruh game yang tersedia di dalam sistem.
 */
     printf("Berikut adalah daftar game yang tersedia :\n");
-    for (int i = IdxMin; i <= List_Game.Neff; i++) {
-        printf("%d. ",i);
-        //printf("%s\n",List_Game.TI[i]);
-        PrintKata(List_Game.TI[i]);
-        printf("\n");
-    }
+    TulisIsi(List_Game);
 }
