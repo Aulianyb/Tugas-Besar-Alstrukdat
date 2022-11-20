9596f7352cb5f84c218b385fd6eeaea59422e17f
Aulia Nadhirah Yasmin Badrulkamal <Aulianybk123@gmail.com>
2022-11-20 17:32:13 +0700

    WIP STACK
    buat stack

---
+++ src/Function/ADT/stack.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include "stack.h"
+
+/* ************ Prototype ************ */
+/* *** Konstruktor/Kreator *** */
+void CreateEmpty(Stack *S){
+	Top(*S) = Nil;
+}
+/* I.S. sembarang; */
+/* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxEl */
+/* jadi indeksnya antara 0.. MaxEl */
+/* Ciri stack kosong : TOP bernilai Nil */
+
+/* ************ Predikat Untuk test keadaan KOLEKSI ************ */
+boolean IsEmpty(Stack S){
+	return (Top(S) == Nil);
+}
+/* Mengirim true jika Stack kosong: lihat definisi di atas */
+boolean IsFull(Stack S){
+	return (Top(S) == MaxEl-1);
+}
+/* Mengirim true jika tabel penampung nilai elemen stack penuh */
+
+/* ************ Menambahkan sebuah elemen ke Stack ************ */
+void Push(Stack * S, infotype X){
+	if (IsEmpty(*S)){
+		Top(*S) = 0;
+		InfoTop(*S) = X;
+	}
+	else{
+		Top(*S)++;
+		InfoTop(*S) = X;
+	}
+}
+/* Menambahkan X sebagai elemen Stack S. */
+/* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
+/* F.S. X menjadi TOP yang baru,TOP bertambah 1 */
+
+/* ************ Menghapus sebuah elemen Stack ************ */
+void Pop(Stack * S, infotype* X){
+	*X = InfoTop(*S);
+	Top(*S)--;
+}
+/* Menghapus X dari Stack S. */
+/* I.S. S  tidak mungkin kosong */
+/* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
+
+infotype makeInfo(int n){	
+	infotype I;
+	if (n == 1){
+		n(I) = 1;
+		graph(I) = strToWord("    *    ");
+	}
+	else if (n == 2){
+		n(I) = 2;
+		graph(I) = strToWord("   ***   ");
+	}
+	else if (n == 3){
+		n(I) = 3;
+		graph(I) = strToWord("  *****  ");
+	}
+	else if (n == 4){
+		n(I) = 4;
+		graph(I) = strToWord(" ******* ");
+	}
+	else if (n == 5){
+		n(I) = 5;
+		graph(I) = strToWord("*********");
+	}
+	return I; 
+}
