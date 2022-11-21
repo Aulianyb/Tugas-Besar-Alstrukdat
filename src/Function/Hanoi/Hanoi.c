32f169e5c259d87b270ce065ccdc1248cd82a991
Aulia Nadhirah Yasmin Badrulkamal <Aulianybk123@gmail.com>
2022-11-21 11:05:15 +0700

    added validasi input
    - validasi input untuk hanoi
    - belum ada end condition tho
    - belum ada score juga
    
    dua diatas itu to do selanjutnya

--- src/Function/Hanoi/Hanoi.c
+++ src/Function/Hanoi/Hanoi.c
@@ -4,7 +4,6 @@
 
 void printTiang(Stack S1, Stack S2, Stack S3){
     for (int i=4;i>=0;i--){
-        printf("%d",Top(S1));
         if (i > Top(S1)){
             printf("    |    ");
         }
@@ -12,7 +11,6 @@ void printTiang(Stack S1, Stack S2, Stack S3){
             PrintKata(graph(S1.T[i]));
         }
         
-        printf("%d",Top(S2));
         if (i > Top(S2)){
             printf("    |    ");
         }
@@ -20,7 +18,6 @@ void printTiang(Stack S1, Stack S2, Stack S3){
             PrintKata(graph(S2.T[i]));
         }
 
-        printf("%d",Top(S3));
         if (i > Top(S3)){
             printf("    |    ");
         }
@@ -32,6 +29,19 @@ void printTiang(Stack S1, Stack S2, Stack S3){
     }
 }
 
+Stack *AssignTiang(Word input, Stack *S1, Stack *S2, Stack *S3){
+    if ((WordToInt(input)) == 1){
+            return S1;
+        }
+        else if((WordToInt(input)) == 2){
+            return S2;
+        }
+        else if((WordToInt(input)) == 3){
+            return S3;
+        }
+}
+
+/* MOVE ELEMEN DARI S1 KE S2 */
 void MoveTower(Stack *S1, Stack *S2){
     infotype temp;
     Pop(S1, &temp);
@@ -58,33 +68,42 @@ int main(){
     //input
     Word Source, Dest; 
     infotype temp;
+    Stack *SourceTiang, *DestTiang;
     Stack Tiang1, Tiang2, Tiang3; 
+    boolean play=true;
     CreateEmpty(&Tiang1);
     CreateEmpty(&Tiang2);
     CreateEmpty(&Tiang3);
-
     //ISI TIANG 1
     for (int i=5;i>=1;i--){
         Push(&Tiang1, makeInfo(i));
     }
 
-    //ISI TIANG 2, 3
-
-    // printf("TIANG ASAL : ");
-    // STARTFILE();
-    // Source = currentWord;
-    // printf("TIANG TUJUAN : ");
-    // STARTFILE();
-    // Dest = currentWord;
-
-
-    printf("BEFORE : \n");
-    printTiang(Tiang1, Tiang2, Tiang3);
-    MoveTower(&Tiang1, &Tiang2);
-    MoveTower(&Tiang1, &Tiang3);
-    MoveTower(&Tiang1, &Tiang3);
-    printf("AFTER\n");
-    printTiang(Tiang1, Tiang2, Tiang3);
+    while (play){
+        boolean valid=false;
+        printTiang(Tiang1, Tiang2, Tiang3);
+        while (!valid){
+            printf("TIANG ASAL : ");
+            STARTFILE();
+            Source = currentWord;
+            printf("TIANG TUJUAN : ");
+            STARTFILE();
+            Dest = currentWord;
+            if ((1 > WordToInt(Source)) || (WordToInt(Source) > 3) || (1 > WordToInt(Dest)) || (WordToInt(Dest) > 3)){
+                printf("-- INPUT TIDAK VALID! MASUKKAN 1, 2 ATAU 3! --\n");
+            }
+            else{
+                if (IsEmpty(*AssignTiang(Source, &Tiang1, &Tiang2, &Tiang3))){
+                    printf("-- INPUT TIDAK VALID! TIANG ASAL YANG DIPILIH KOSONG! --\n");
+                }
+                else{
+                    valid=true;
+                }
+            }
+        }
+        
+        MoveTower(AssignTiang(Source, &Tiang1, &Tiang2, &Tiang3), AssignTiang(Dest, &Tiang1, &Tiang2, &Tiang3) );
+    }
 
     return 0;
 }
