b998d2d9e53c299e9010f90522f8d54fe84278ae
Aulia Nadhirah Yasmin Badrulkamal <Aulianybk123@gmail.com>
2022-11-22 21:52:39 +0700

    Added Algorithm
    it's playable, but no score

--- src/Function/Hanoi/Hanoi.c
+++ src/Function/Hanoi/Hanoi.c
@@ -4,12 +4,16 @@
 
 void printTiang(Stack S1, Stack S2, Stack S3){
     for (int i=4;i>=0;i--){
+        printf("   ");
+
         if (i > Top(S1)){
             printf("    |    ");
         }
         else{
             PrintKata(graph(S1.T[i]));
         }
+
+        printf("       ");
         
         if (i > Top(S2)){
             printf("    |    ");
@@ -17,6 +21,8 @@ void printTiang(Stack S1, Stack S2, Stack S3){
         else{
             PrintKata(graph(S2.T[i]));
         }
+
+        printf("       ");
 
         if (i > Top(S3)){
             printf("    |    ");
@@ -29,16 +35,19 @@ void printTiang(Stack S1, Stack S2, Stack S3){
     }
 }
 
-Stack *AssignTiang(Word input, Stack *S1, Stack *S2, Stack *S3){
-    if ((WordToInt(input)) == 1){
-            return S1;
+boolean isTowerOrdered(Stack S){
+    boolean ordered=true;
+    if (Top(S) == 4){
+        for (int i=0;i<Top(S);i++){
+            if(n(S.T[i]) < n(S.T[i+1])){
+                ordered = false;
+            }
         }
-        else if((WordToInt(input)) == 2){
-            return S2;
-        }
-        else if((WordToInt(input)) == 3){
-            return S3;
-        }
+    }
+    else{
+        ordered = false;
+    }
+    return ordered;
 }
 
 /* MOVE ELEMEN DARI S1 KE S2 */
@@ -50,20 +59,20 @@ void MoveTower(Stack *S1, Stack *S2){
 
 int main(){
     printf("\n\n");
-    printf("+ = + = + = + = + = + = + = + = + = + = + = + =\n\n");
-    printf("          _____ _____      _____ ___ \n");
-    printf("         |_   _/ _ \\ \\    / / __| _ \\ \n");
-    printf("           | || (_) \\ \\/\\/ /| _||   / \n");
-    printf("           |_| \\___/ \\_/\\_/ |___|_|_\\ \n");
-    printf("                 / _ \\| __|           \n");
-    printf("                | (_) | _|            \n");
-    printf("           _  _  \\___/|_|_  ___ ___   \n");
-    printf("          | || | /_\\ | \\| |/ _ \\_ _| \n");
-    printf("          | __ |/ _ \\| .` | (_) | | \n");
-    printf("          |_||_/_/ \\_\\_|\\_|\\___/___| \n\n");
+    printf("+-----------------------------------------------+\n");
+    printf("         _____ _____      _____ ___ \n");
+    printf("        |_   _/ _ \\ \\    / / __| _ \\ \n");
+    printf("          | || (_) \\ \\/\\/ /| _||   / \n");
+    printf("          |_| \\___/ \\_/\\_/ |___|_|_\\ \n");
+    printf("                / _ \\| __|           \n");
+    printf("               | (_) | _|            \n");
+    printf("          _  _  \\___/|_|_  ___ ___   \n");
+    printf("         | || | /_\\ | \\| |/ _ \\_ _| \n");
+    printf("         | __ |/ _ \\| .` | (_) | | \n");
+    printf("         |_||_/_/ \\_\\_|\\_|\\___/___| \n\n");
 
-    printf("+ = + = + = + = + = + = + = + = + = + = + = + =\n");
-    printf("\n\n");
+    printf("+-----------------------------------------------+\n");
+    printf("\n");
     
     //input
     Word Source, Dest; 
@@ -74,14 +83,40 @@ int main(){
     CreateEmpty(&Tiang1);
     CreateEmpty(&Tiang2);
     CreateEmpty(&Tiang3);
-    //ISI TIANG 1
-    for (int i=5;i>=1;i--){
-        Push(&Tiang1, makeInfo(i));
+
+    Stack *AssignTiang(Word input, Stack *S1, Stack *S2, Stack *S3){
+    if ((WordToInt(input)) == 1){
+            return S1;
+        }
+        else if((WordToInt(input)) == 2){
+            return S2;
+        }
+        else if((WordToInt(input)) == 3){
+            return S3;
+        }
     }
+
+    //ISI TIANG 1
+    // for (int i=5;i>=1;i--){
+    //     Push(&Tiang1, makeInfo(i));
+    //     Push(&Tiang2, makeInfo(i));
+    //     Push(&Tiang3, makeInfo(i));
+
+    // }
+
+    //DEBUGGING MODE
+    for (int i=5;i>=2;i--){
+        Push(&Tiang2, makeInfo(i));
+    }
+    Push(&Tiang1, makeInfo(1));
+
 
     while (play){
         boolean valid=false;
         printTiang(Tiang1, Tiang2, Tiang3);
+        printf("+-----------------------------------------------+\n");
+        printf("|       1       |       2       |       3       |\n");
+        printf("+-----------------------------------------------+\n");
         while (!valid){
             printf("TIANG ASAL : ");
             STARTFILE();
@@ -97,13 +132,23 @@ int main(){
                     printf("-- INPUT TIDAK VALID! TIANG ASAL YANG DIPILIH KOSONG! --\n");
                 }
                 else{
-                    valid=true;
+                    if (!(IsEmpty(*AssignTiang(Dest, &Tiang1, &Tiang2, &Tiang3)))&&(n(InfoTop(*AssignTiang(Source, &Tiang1, &Tiang2, &Tiang3))) > n(InfoTop(*AssignTiang(Dest, &Tiang1, &Tiang2, &Tiang3))))){
+                        printf("-- DISK YANG DIPINDAHKAN HARUS LEBIH KECIL --\n");
+                    }
+                    else{
+                        valid=true;                    
+                    }
                 }
             }
         }
         
-        MoveTower(AssignTiang(Source, &Tiang1, &Tiang2, &Tiang3), AssignTiang(Dest, &Tiang1, &Tiang2, &Tiang3) );
+        MoveTower(AssignTiang(Source, &Tiang1, &Tiang2, &Tiang3), AssignTiang(Dest, &Tiang1, &Tiang2, &Tiang3));
+        if (isTowerOrdered(Tiang2) || isTowerOrdered(Tiang3)){
+            play=false;
+        }
+        printf("+-----------------------------------------------+\n");
     }
-
+    printf("GAME OVER!\n");
+    printTiang(Tiang1, Tiang2, Tiang3);
     return 0;
 }
