aa221c8c84e663a0303b4d8f4a812b2ef31c0561
Aulia Nadhirah Yasmin Badrulkamal <Aulianybk123@gmail.com>
2022-12-01 21:37:44 +0700

    ADT Stack Selesai Drivernya
    OMG SELESAI!!

--- src/Driver/ADT_Stack.c
+++ src/Driver/ADT_Stack.c
@@ -3,8 +3,25 @@
 #include <stdio.h>
 
 int printStack(Stack S, int n){
+    
     for (int i=n-1;i>0;i--){
-        PrintKata(graph(S.T[i]));
+        if (i > Top(S)){
+            int j=0;
+            while (j<(2*n-1)/2){
+                printf(" ");
+                // printf("%d", n);
+                j++;    
+            }
+            printf("|");
+            j++;
+            while (j<(2*n-1)){
+                printf(" ");
+                j++;    
+            }
+        }
+        else{
+            PrintKata(graph(S.T[i]));
+        }
         printf("\n");
     }
 }
@@ -23,15 +40,25 @@ int main(){
     printf("==== isiTiang ====\n");
     printf("Isi Tiang akan mengisi Tiang sesuai input n\n");
     printf("Misal : N = 10\n");
-    isiTiang(&S, 27);
-    printStack(S, 27);
+    isiTiang(&S, 24);
+    printStack(S, 24);
+    printf("==== IsFull_S ====\n");
+    printf("Return nilai apakah stack memenuhi kapasitas atau tidak (MaxEL : 24)\n");
+    printf("isFull : %d", IsFull_S(S));
     printf("\n");
     printf("Misal : N = 5\n");
     CreateEmpty(&S);
     isiTiang(&S, 5);
     printStack(S, 5);
-    printf("\n");
-    printf("==== Push ====\n");
     printf("==== Pop ====\n");
+    infotype temp; 
+    Pop(&S, &temp);
+    printf("ELEMEN YANG DI POP :\nn : %d\ngraph : ", n(temp));
+    PrintKata(graph(temp));
+    printf("\n\n");
+    printStack(S, 5);
+    printf("==== Push ====\n");
+    Push(&S, temp);
+    printStack(S, 5);
     return 0;
 }
